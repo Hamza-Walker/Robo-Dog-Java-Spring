add70dcb299b18daad4c6928f4cbb5ad7e85e3e9
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-09-16 23:22:29 +0200

    Dogs are stored in an ordered collection. A dog can be added to the collection. A randomly generated dog can be added to the collection. The collection of all dogs can be returned. The first dog with a given name can have its age and breed properties updated.

--- src/main/java/com/walker/model/Dog.java
+++ src/main/java/com/walker/model/Dog.java
@@ -1,7 +1,57 @@
 package com.walker.model;
 
-public record Dog (String name, int age, DogBreed breed) {
+import java.util.Objects;
 
+public class Dog {
 
+        private String name;
+        private int age;
+        private DogBreed breed;
 
+    public Dog(String name, int age, DogBreed breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public DogBreed getBreed() {
+        return breed;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setBreed(DogBreed breed) {
+        this.breed = breed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Dog dog)) return false;
+        return age == dog.age && Objects.equals(name, dog.name) && breed == dog.breed;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, breed);
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", breed=" + breed +
+                '}';
+    }
 }
---
+++ src/main/java/com/walker/service/DogStorage.java
@@ -0,0 +1,34 @@
+package com.walker.service;
+
+import com.walker.model.Dog;
+import com.walker.model.DogBreed;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DogStorage {
+
+    private List<Dog> allDogsList;
+
+    public DogStorage(List<Dog> dogStorage) {
+        this.allDogsList = new ArrayList<>();
+    }
+    public void addDog (Dog dog){
+        allDogsList.add(dog);
+    }
+
+    public List<Dog> getAllDogsList() {
+        return allDogsList;
+    }
+
+    public Dog updateDog (String name, int newAge , DogBreed updatedBreed){
+        for ( Dog dog : allDogsList){
+            if ( dog.getName().equals(name)) {
+                dog.setAge(newAge);
+                dog.setBreed(updatedBreed);
+                break;
+            }
+        }
+    }
+
+}
